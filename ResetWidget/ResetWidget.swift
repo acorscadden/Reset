22eea4486179f85ff22970872d5388ea86b59cdc
Adrian Corscadden <me@acorscadden.com>
2024-10-06 15:31:47 -0700

    Timeline tweaks

--- ResetWidget/ResetWidget.swift
+++ ResetWidget/ResetWidget.swift
@@ -18,11 +18,11 @@ struct Provider: TimelineProvider {
   }
 
   func placeholder(in context: Context) -> SimpleEntry {
-    SimpleEntry(date: Date())
+    SimpleEntry(date: Date(), displayDate: .now)
   }
 
   func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-    let entry = SimpleEntry(date: Date())
+    let entry = SimpleEntry(date: Date(), displayDate: .now)
     completion(entry)
   }
 
@@ -32,35 +32,20 @@ struct Provider: TimelineProvider {
       if let resets = try? await container.mainContext.fetch(descriptor) {
         if !resets.isEmpty {
           let lastReset = resets.last!
-          let entry = SimpleEntry(date: lastReset.date)
-          let timeline = Timeline(entries: [entry], policy: .atEnd)
-          completion(timeline)
-        } else {
-          var entries: [SimpleEntry] = []
-
-          // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-          let currentDate = Date()
-          for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+          var entries = [SimpleEntry]()
+          for minute in 0 ..< 60 {
+            let entryDate = Calendar.current.date(byAdding: .minute, value: minute, to: .now)!
+            let entry = SimpleEntry(date: entryDate, displayDate: lastReset.date)
             entries.append(entry)
           }
-
           let timeline = Timeline(entries: entries, policy: .atEnd)
+          completion(timeline)
+        } else {
+          let timeline = Timeline(entries: [SimpleEntry(date: .now, displayDate: .now)], policy: .atEnd)
           completion(timeline)
         }
       } else {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-          let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-          let entry = SimpleEntry(date: entryDate)
-          entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: [SimpleEntry(date: .now, displayDate: .now)], policy: .atEnd)
         completion(timeline)
       }
 
@@ -74,13 +59,14 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
   let date: Date
+  let displayDate: Date
 }
 
 struct ResetWidgetEntryView : View {
   var entry: Provider.Entry
 
   var body: some View {
-    TimeSinceView(startDate: entry.date)
+    TimeSinceView(startDate: entry.displayDate)
   }
 }
 
@@ -100,6 +86,6 @@ struct ResetWidget: Widget {
 #Preview(as: .systemSmall) {
   ResetWidget()
 } timeline: {
-  SimpleEntry(date: .now)
-  SimpleEntry(date: .now)
+  SimpleEntry(date: .now, displayDate: .now)
+  SimpleEntry(date: .now, displayDate: .now)
 }
