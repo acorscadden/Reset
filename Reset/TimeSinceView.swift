33b75896a1423282b247bb1d798ed128d31a33a0
Adrian Corscadden <me@acorscadden.com>
2024-10-09 08:56:34 -0700

    Custom progress views

--- Reset/TimeSinceView.swift
+++ Reset/TimeSinceView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+struct LinearProgressView: View {
+  let label: String
+  let value: Double
+  let maxValue: Double
+  let height = 44.0
+
+  var body: some View {
+    Capsule().fill(.gray)
+      .overlay {
+        ZStack(alignment: .leading) {
+          GeometryReader { proxy in
+            Rectangle().fill(.tint)
+              .frame(width: proxy.size.width * (value / maxValue))
+          }
+          Text("\(Int(value)) \(label)")
+            .foregroundStyle(.white)
+            .padding(.leading, 8)
+        }
+      }
+      .clipShape(Capsule())
+      .frame(height: height)
+  }
+}
+
 struct TimeSinceView: View {
   let startDate: Date
   @State private var now = Date()
@@ -14,25 +38,31 @@ struct TimeSinceView: View {
 
   private func timeElapsedComponents(from startDate: Date) -> (days: Int, hours: Int, minutes: Int, seconds: Int) {
     let calendar = Calendar.current
-    let components = calendar.dateComponents([.day, .hour, .minute, .second], from: startDate, to: now)
+    let components = calendar.dateComponents(
+      [.day, .hour, .minute, .second],
+      from: startDate,
+      to: now
+    )
     return (days: components.day ?? 0, hours: components.hour ?? 0, minutes: components.minute ?? 0, seconds: components.second ?? 0)
   }
 
   var body: some View {
     let timeComponents = timeElapsedComponents(from: startDate)
     VStack(alignment: .leading, spacing: 10) {
-      ProgressBar(label: "Days", value: Double(timeComponents.days), maxValue: 365)
+      LinearProgressView(label: "Weeks", value: Double(timeComponents.days/7), maxValue: 52)
+        .tint(.five)
+      LinearProgressView(label: "Days", value: Double(timeComponents.days%7), maxValue: 7)
         .tint(.four)
-      ProgressBar(label: "Hours", value: Double(timeComponents.hours), maxValue: 24)
+      LinearProgressView(label: "Hours", value: Double(timeComponents.hours), maxValue: 24)
         .tint(.three)
-      ProgressBar(label: "Minutes", value: Double(timeComponents.minutes), maxValue: 60)
+      LinearProgressView(label: "Minutes", value: Double(timeComponents.minutes), maxValue: 60)
         .tint(.two)
-      ProgressBar(label: "Seconds", value: Double(timeComponents.seconds), maxValue: 60)
+      LinearProgressView(label: "Seconds", value: Double(timeComponents.seconds), maxValue: 60)
         .tint(.one)
     }
     .padding()
     .onReceive(timer) { _ in
-      now = Date()  // Update the current time
+      now = Date()
     }
   }
 }
@@ -47,11 +77,12 @@ struct ProgressBar: View {
       Text("\(label): \(Int(value))")
         .font(.caption)
       ProgressView(value: max(value, 0), total: maxValue)
-        .progressViewStyle(LinearProgressViewStyle())
+        .progressViewStyle(.linear)
+        .controlSize(.extraLarge)
     }
   }
 }
 
 #Preview {
-  TimeSinceView(startDate: Date())
+  TimeSinceView(startDate: StartDate)
 }
